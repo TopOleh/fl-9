23298ae101a8680cce8da35dfd0403001e512107
Oleh Topchak <dgtop997@gmail.com>
2018-08-25 22:31:55 +0300

    Valid nine homework

---
+++ FE_9_9_homework_data-types/homework/index.js
@@ -0,0 +1,60 @@
+function findType(param){
+  return typeof param;
+}
+
+function forEach(arr, func) {
+  for (let elem of arr) {
+    func(elem);
+  }
+}
+
+function map (arr, func) {
+  let mappedArr = [];
+  forEach( arr, elem => mappedArr.push(func(elem)));
+  return mappedArr;
+}
+
+function filter(arr, func) {
+  let filteredArr = [];
+  forEach(arr, elem => {
+    if (func(elem)) {
+      filteredArr.push(elem);
+    }
+  });
+  return filteredArr;
+}
+
+function getAdultAppleLovers(data) {
+  let arr = [];
+  map(filter(data, elem => {
+    if (elem['age'] > 18 && elem['favoriteFruit'] === 'apple'){
+      arr.push(elem['name']);
+    }
+  }));
+  return arr;
+}
+
+function keys(array) {
+  let arr = [];
+  for ( let keys in array ) {
+    if (array.hasOwnProperty(keys)) {
+      arr.push(keys);
+    }
+  }
+  return arr;
+}
+
+function values(array) {
+  let arr = [];
+  for (let values in array) {
+    if (array.hasOwnProperty(values)) {
+      arr.push(array[values]);
+    }
+  }
+  return arr;
+}
+
+function showFormattedDate(date) {
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  return `It is ${date.getDate()} of ${months[date.getMonth()]}, ${date.getFullYear()}`;
+}
